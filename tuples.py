937ac746b2d601df4e43ef1746fa241fce51cf25
Student <yvonneoyellaa@gmail.com>
2024-04-14 13:57:59 +0300

    include iterate solution

--- tuples.py
+++ tuples.py
@@ -1,6 +1,38 @@
 
 # creating a tuple
-numbers = (10,20,30,40,50)
+my_tuples = (10,20,30,40,50)
 
-# accessing my_tuple tuple,
-print(numbers[4])
+#creating a tuple that has only one item
+number= (5,)
+
+
+#adding items to a tuple.
+my_list =list(number)
+a= (60,70,80)
+my_list = number + a
+numbers= tuple(my_list)
+
+#removing items from a tuple 
+new_list = list(my_tuples)
+my_list= new_list.pop()
+my_tuples= tuple(new_list)
+
+#iterating through a list
+my_list= list(my_tuples)
+
+for i in my_list:print(i+10)
+
+my_tuples= tuple(my_list)
+
+
+
+print(number)
+print(my_tuples)
+
+
+
+# accessing my_tuples tuple,
+print(my_tuples[1])
+
+#accessing the number tuple
+print(number[0])
